3b1ccf48b15fc4d5c3346d9f4c8beca7c0adecf8
Chris Jaramillo <chris.jaramillo@outlook.com>
2019-04-13 12:27:46 -0700

    ATTACKIQ: - Adding some boost - C++14 auto return type!

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 //
 
 #include <iostream>
+#include <boost/filesystem.hpp>
 
-int main(){
+int main(int argc, char *argv[]){
     std::cout << "Hello World" << std::endl;
+    boost::filesystem::path aPath = boost::filesystem::current_path();
+    boost::filesystem::directory_iterator it(aPath);
+    while(it != boost::filesystem::directory_iterator())
+    {
+        std::cout << *it++ << std::endl;
+    }
     return EXIT_SUCCESS;
 }
